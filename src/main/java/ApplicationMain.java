b3d5ede50912b3e86c12a260a5fdeb2b08ed3c8c
pqrsumit <pqrsumit@gmail.com>
2019-07-29 20:56:43 -0400

    Added Dependencies for Kafka and HDFS

--- src/main/java/ApplicationMain.java
+++ src/main/java/ApplicationMain.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.logging.Logger;
 
 public class ApplicationMain {
+
+    static Logger toLog = Logger.getLogger(ApplicationMain.class.getName());
+    public static void main(String[] args) {
+
+        toLog.info("This message is from Application Main" +
+                ":: Hello World...");
+
+    }
 }
